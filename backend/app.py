d6d5ec811e3de38dd34cb82adcf1d9b34fee052d
Siddhant Dongre <siddhantdongre50@gmail.com>
2025-02-23 22:15:14 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -75,5 +75,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if no port is specified
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000
+    app.run(host="0.0.0.0", port=port)
