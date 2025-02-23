a0ab88838d135012f2b710581fee858a1e8f4d81
Siddhant Dongre <siddhantdongre50@gmail.com>
2025-02-23 19:10:38 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -74,5 +74,6 @@ def get_positive_advice():
 def home():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if no port is specified
+    app.run(host='0.0.0.0', port=port)
